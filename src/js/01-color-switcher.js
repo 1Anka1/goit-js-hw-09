3d368c63cc85e1dbfe08e6e2cb65c2c040f48e64
Your Name <97912793+1Anka1@users.noreply.github.com>
2022-07-31 13:12:35 +0200

    Update task 1 - color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+const SET_INTERVAL = 1000;
+let timerId = null;
+
+refs.startBtn.addEventListener('click', setChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setChangeColor() {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor();
+    }, SET_INTERVAL); 
+    refs.startBtn.setAttribute ('disabled', 'disabled');
+    refs.stopBtn.removeAttribute ('disabled')
+}
+
+function stopChangeColor() {
+    clearInterval(timerId);
+    refs.stopBtn.setAttribute ('disabled', 'disabled')
+    refs.startBtn.removeAttribute ('disabled')
+}
